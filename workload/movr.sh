796526b87c1a5e38830957fcbd4ce163a0c65a6f
duanelupinski <duane.lupinski@cockroachlabs.com>
2025-10-01 15:15:31 -0400

    updating movr.sh

--- workload/movr.sh
+++ workload/movr.sh
@@ -1,10 +1,22 @@
-# assumes cockroach client installed
+#!/bin/bash
+
+# assumes cockroach cli client installed
+#  drop and recreates movr db
+
+read -sp "Enter password: " PASSWORD
+
+export PGPASSWORD="$PASSWORD"
+
+cockroach sql --url 'postgresql://demo@duane-demo-standard-16600.j77.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full' <<EOF
+drop database movr;
+create database movr;
+EOF
 
 cockroach workload init movr \
-  'postgresql://<user>:<password>@<host>:26257/<db>?sslmode=require'
+  'postgresql://demo@duane-demo-standard-16600.j77.aws-us-east-1.cockroachlabs.cloud:26257/movr?sslmode=verify-full'
 
 cockroach workload run movr \
   --duration=5m \
-  --concurrency=64 \
+  --concurrency=12 \
   --tolerate-errors \
-  'postgresql://<user>:<password>@<host>:26257/<db>?sslmode=require'
+  'postgresql://demo@duane-demo-standard-16600.j77.aws-us-east-1.cockroachlabs.cloud:26257/movr?sslmode=verify-full'
